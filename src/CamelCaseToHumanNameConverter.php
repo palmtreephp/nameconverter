70de03546885a1d70fb259e602c2431397fa78ff
andyexeter <palmer.andy@gmail.com>
2025-12-02 10:04:58 +0000

    Add CamelCaseToHumanNameConverter

---
+++ src/CamelCaseToHumanNameConverter.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Palmtree\NameConverter;
+
+class CamelCaseToHumanNameConverter implements NameConverterInterface
+{
+    public function normalize(string $input): string
+    {
+        // Insert spaces before uppercase letters and trim
+        $spaced = preg_replace('/([A-Z])/', ' $1', $input);
+        return ucwords(trim($spaced));
+    }
+
+    public function denormalize(string $input): string
+    {
+        // Remove spaces and convert to camelCase
+        $words = explode(' ', trim($input));
+        $result = strtolower(array_shift($words));
+        foreach ($words as $word) {
+            $result .= ucfirst(strtolower($word));
+        }
+        return $result;
+    }
+}
+
